116eae8e1ef73687406b2af1de391d659ba1482c
Shagun Kazan <shagun26@hotmail.com>
2018-09-03 14:33:43 -0400

    updated to handle css js png

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
 
 var server = http.createServer((req, res) => 
 {
-    if (req.url === '/' || req.url.includes('/?'))
-    {
+
     console.log("Request was made: " + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/main.html').pipe(res);
+    
+    if (req.url === '/')
+    {
+        fs.readFile('./public/main.html', 'UTF-8', (err, html) => 
+        {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(html);
+
+        });
+    }
+    else if (req.url.match("\.css$"))
+    {
+        var cssPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        fileStream.pipe(res);
+    }
+    else if (req.url.match("\.png$"))
+    {
+        var imgPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(imgPath, "UTF-8");
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        fileStream.pipe(res);
+    }
+    else if (req.url.match("\.js$"))
+    {
+        var jsPath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(jsPath, "UTF-8");
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        fileStream.pipe(res);
+    }
+    else
+    {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("Page Not Found!");
     }
 
-});
-
-server.listen(8000);
+}).listen(8000);
